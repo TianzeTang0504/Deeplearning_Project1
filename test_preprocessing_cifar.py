6517180d6e07bdfbb4bfd0445be8a6fd757f958c
TianzeTang <tt2631@nyu.edu>
2025-03-13 14:54:10 -0400

    Update test_preprocessing_cifar.py

--- test_preprocessing_cifar.py
+++ test_preprocessing_cifar.py
@@ -10,9 +10,7 @@ def unpickle(file):
 test_file = "./data/deep-learning-spring-2025-project-1/cifar-10-python/cifar-10-batches-py/test_batch"
 test = unpickle(test_file)
 
-# 重新调整数据形状: (50000, 3072) → (50000, 32, 32, 3)
 images = test[b'data'].reshape(-1, 3, 32, 32)
 
-# 保存转换后的数据
-np.save("test_images.npy", images)   # 保存图像数据
-np.save("test_labels.npy", test[b'labels'])  # 保存标签
+np.save("test_images.npy", images)
+np.save("test_labels.npy", test[b'labels'])
