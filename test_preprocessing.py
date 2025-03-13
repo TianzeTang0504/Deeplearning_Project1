5a6113743d266a59c72b96d55da737534a0e9d6e
TianzeTang <tt2631@nyu.edu>
2025-03-13 14:56:06 -0400

    Update test_preprocessing.py

--- test_preprocessing.py
+++ test_preprocessing.py
@@ -12,22 +12,19 @@ test = unpickle(test_file)
 
 test_images = test[b'data']
 test_images = test_images.transpose(0, 3, 1, 2)
-test_ids = test[b'ids']  # 确保获取 id 信息
+test_ids = test[b'ids']
 np.savez("test_data.npz", images=test_images, ids=test_ids)
 
 
-# 加载.npy文件
-file_path = "test_data.npz"  # 替换为你的.npy文件路径
-test_data = np.load(file_path)  # 形状为 (N, 32, 32, 3)
+file_path = "test_data.npz"
+test_data = np.load(file_path)
 images = test_data["images"]
 ids = test_data["ids"]
 
-# 随机选择一张图片
-random_index = np.random.randint(0, images.shape[0])  # 生成随机索引
-random_image = np.transpose(images[random_index], (1, 2, 0))  # 获取随机图片
+random_index = np.random.randint(0, images.shape[0])
+random_image = np.transpose(images[random_index], (1, 2, 0))
 
-# 显示图片
-plt.imshow(random_image.astype(np.uint8))  # 确保图片数据类型为 uint8
-plt.axis("off")  # 隐藏坐标轴
-plt.title(f"Random Image Index: {random_index}")  # 显示索引
+plt.imshow(random_image.astype(np.uint8))
+plt.axis("off")
+plt.title(f"Random Image Index: {random_index}")
 plt.show()
